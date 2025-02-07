cec14c9cc08c97f3b483f66ed8241bc3ecd8c39c
vorvikqq <delodonn@ukr.net>
2025-02-08 01:53:17 +0200

    add pausing

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from pacman import Pacman
 from nodes import NodeGroup
 from pellets import PelletGroup
 from fruit import Fruit
-from ghosts import Ghost, GhostsGroup
+from ghosts import GhostsGroup
+from pauser import Pause
 
 class GameController(object):
     def __init__(self):
@@ -14,6 +15,7 @@ class GameController(object):
         self.background = None
         self.clock = pygame.time.Clock()
         self.fruit = None
+        self.pause = Pause(True)
 
     def setBackground(self):
         self.background = pygame.surface.Surface(SCREENSIZE).convert()
@@ -30,24 +32,26 @@ class GameController(object):
         self.nodes.connectHomeNodes(homekey, (15,14), RIGHT)
 
         self.pacman = Pacman(self.nodes.getStartTempNode())
-        # self.ghosts = GhostsGroup(self.nodes.getStartTempNode(), self.pacman)
-        # self.ghosts.blinky.set_spawn_node(self.nodes.getNodeFromTiles(2+11.5, 14))
-        # self.ghosts.pinky.set_spawn_node(self.nodes.getNodeFromTiles(2+11.5, 3+14))
-        # self.ghosts.inky.set_spawn_node(self.nodes.getNodeFromTiles(0+11.5, 3+14))
-        # self.ghosts.clyde.set_spawn_node(self.nodes.getNodeFromTiles(4+11.5, 3+14))
         self.ghosts = GhostsGroup(self.nodes.getStartTempNode(), self.pacman)
-        self.ghosts.set_spawn_node(self.nodes.getNodeFromTiles(2+11.5, 3+14))
+        self.ghosts.blinky.set_spawn_node(self.nodes.getNodeFromTiles(2+11.5, 14))
+        self.ghosts.pinky.set_spawn_node(self.nodes.getNodeFromTiles(2+11.5, 3+14))
+        self.ghosts.inky.set_spawn_node(self.nodes.getNodeFromTiles(0+11.5, 3+14))
+        self.ghosts.clyde.set_spawn_node(self.nodes.getNodeFromTiles(4+11.5, 3+14))
 
     def update(self):
         dt = self.clock.tick(60) / 1000.0
-        self.pacman.update(dt)
-        self.ghosts.update(dt)
         self.pelletGroup.update(dt)
-        if self.fruit is not None:
-            self.fruit.update(dt)
-        self.checkPelletEvents()
-        self.checkFruitEvents()
-        self.checkGhostEvents()
+        if not self.pause.paused:
+            self.pacman.update(dt)
+            self.ghosts.update(dt)
+            if self.fruit is not None:
+                self.fruit.update(dt)
+            self.checkPelletEvents()
+            self.checkFruitEvents()
+            self.checkGhostEvents()
+        after_pause_method = self.pause.update(dt)
+        if after_pause_method is not None:
+            after_pause_method()
         self.checkEvents()
         self.render()
     
@@ -74,13 +78,31 @@ class GameController(object):
         for event in pygame.event.get():
             if(event.type == QUIT):
                 exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    self.pause.set_pause(player_paused=True)
+                    if not self.pause.paused:
+                        self.show_entities
+                    else:
+                        self.hide_entities()
 
     def checkGhostEvents(self):
         for ghost in self.ghosts:
             if self.pacman.collide_ghost(ghost):
                 if ghost.mode.current_mode is FREIGHT:
+                    self.pacman.visible = False
+                    ghost.visible = False
+                    self.pause.set_pause(pause_time=1, func=self.show_entities)
                     ghost.start_spawn()
 
+    def show_entities(self):
+        self.pacman.visible = True
+        self.ghosts.show()
+    
+    def hide_entities(self):
+        self.pacman.visible = False
+        self.ghosts.hide()
+    
     def render(self):
         self.screen.blit(self.background, (0, 0))
         self.nodes.render(self.screen)
