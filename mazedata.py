a1edcc64b22ce9ff7dc6269fb4a7cdca6adca2f9
vorvikqq <delodonn@ukr.net>
2025-02-12 23:22:25 +0200

    add specific mazes likes Maze1 and Maze2

--- mazedata.py
+++ mazedata.py
@@ -26,3 +26,30 @@ class MazeBase:
       for values in self.ghost_node_deny[direction]:
         nodes.denyAccessList(*(values + (direction, ghosts)))
 
+
+class Maze1(MazeBase):
+  def __init__(self) -> None:
+    super().__init__(self)
+    self.name = "maze1"
+    self.portal_pairs =  {0:((0, 17), (27, 17))}
+    self.home_offset = (11.5, 14)
+    self.home_node_connect_left = (12, 14)
+    self.home_node_connect_right = (15, 14)
+    self.pacman_start = (15, 26)
+    self.fruit_start = (9, 20)
+    self.ghost_node_deny =  {UP:((12, 14), (15, 14), (12, 26), (15, 26)), LEFT:(self.add_offset(2, 3),),
+                              RIGHT:(self.add_offset(2, 3),)}
+    
+class Maze2(MazeBase):
+    def __init__(self):
+        MazeBase.__init__(self)
+        self.name = "maze2"
+        self.portal_pairs = {0:((0, 4), (27, 4)), 1:((0, 26), (27, 26))}
+        self.home_offset = (11.5, 14)
+        self.home_node_connect_left = (9, 14)
+        self.home_node_connect_right = (18, 14)
+        self.pacman_start = (16, 26)
+        self.fruit_start = (11, 20)
+        self.ghost_node_deny = {UP:((9, 14), (18, 14), (11, 23), (16, 23)), LEFT:(self.add_offset(2, 3),),
+                              RIGHT:(self.add_offset(2, 3),)}
+        
