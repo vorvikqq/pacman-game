45ae8d01ea61b630fd17c0bbdc1fbb011b2eda25
Tony-jdev <artpopko28@gmail.com>
2025-02-13 15:53:09 +0200

    docs: added documentation for Pacman class, unnecessary methods deleted

--- pacman.py
+++ pacman.py
@@ -5,21 +5,31 @@ from constants import *
 from entity import Entity
 from sprites import PacmanSprites
 
+
 class Pacman(Entity):
+    """
+    Represents the Pacman character in the game, handling movement, collisions,
+    and interactions with pellets and ghosts.
+    """
+
     def __init__(self, node):
+        """
+        Initializes the Pacman entity.
+
+        :param node: The starting node for Pacman.
+        """
         super().__init__(node)
         self.name = PACMAN
         self.directions = {
-          STOP : Vector(),
-          UP: Vector(0,-1),
-          DOWN : Vector(0,1),
-          LEFT : Vector(-1,0),
-          RIGHT : Vector(1,0)
+            STOP: Vector(),
+            UP: Vector(0, -1),
+            DOWN: Vector(0, 1),
+            LEFT: Vector(-1, 0),
+            RIGHT: Vector(1, 0)
         }
         self.direction = STOP
-        self.speed = 100 * TILEWIDTH/16
+        self.speed = 100 * TILEWIDTH / 16
         self.radius = 10
-        # self.collideRadius = 5
         self.color = YELLOW
         self.node = node
         self.set_position()
@@ -28,26 +38,33 @@ class Pacman(Entity):
         self.setBetweenNodes(LEFT)
         self.alive = True
         self.sprites = PacmanSprites(self)
-        
-    
+
     def reset(self):
+        """
+        Resets Pacman to its initial state.
+        """
         Entity.reset(self)
         self.direction = LEFT
         self.setBetweenNodes(LEFT)
         self.alive = True
         self.image = self.sprites.get_start_Image()
         self.sprites.reset()
-    
+
     def die(self):
+        """
+        Handles Pacman's death by stopping movement and setting alive to False.
+        """
         self.alive = False
         self.direction = STOP
 
-    # def set_position(self):
-    #     self.position = self.node.position.copy()
-
     def update(self, dt):
+        """
+        Updates Pacman's position and handles movement logic.
+
+        :param dt: Time delta for frame updates.
+        """
         self.sprites.update(dt)
-        self.position += self.directions[self.direction] * self.speed * dt 
+        self.position += self.directions[self.direction] * self.speed * dt
         direction = self.getValidKey()
         if self.overshot_target():
             self.node = self.target
@@ -58,7 +75,7 @@ class Pacman(Entity):
                 self.direction = direction
             else:
                 self.target = self.get_new_target(self.direction)
-            
+
             if self.target is self.node:
                 self.direction = STOP
             self.set_position()
@@ -66,18 +83,12 @@ class Pacman(Entity):
             if self.opposite_direction(direction):
                 self.reverse_direction()
 
-    # def valid_direction(self,direction):
-    #     if direction is not STOP:
-    #         if self.node.neighbors[direction] is not None:
-    #             return True
-    #     return False
-
-    # def get_new_target(self, direction):
-    #     if self.valid_direction(direction):
-    #         return self.node.neighbors[direction]
-    #     return self.node
-
     def getValidKey(self):
+        """
+        Gets the current valid movement key pressed by the player.
+
+        :return: The direction of movement.
+        """
         key_pressed = pygame.key.get_pressed()
         if key_pressed[K_UP]:
             return UP
@@ -88,59 +99,36 @@ class Pacman(Entity):
         if key_pressed[K_RIGHT]:
             return RIGHT
         return STOP
-    
-    
-    # def render(self, screen):
-    #     p = self.position.asInt()
-    #     pygame.draw.circle(screen, self.color, p, self.radius)
-    
-    # def overshot_target(self):
-    #     if self.target is not None:
-    #         vec1 = self.target.position - self.node.position
-    #         vec2 = self.position - self.node.position
-    #         node2_target = vec1.magnitudeSquared()
-    #         node2_self = vec2.magnitudeSquared()
-    #         return node2_self >= node2_target
-    #     return False
-    
-    # def reverse_direction(self):
-    #     self.direction *= -1
-    #     temp = self.node
-    #     self.node = self.target
-    #     self.target = temp
-
-    # def opposite_direction(self, direction):
-    #     if direction is not STOP:
-    #         if direction == self.direction * -1:
-    #             return True
-    #     return False
 
     def eatPellets(self, pelletList):
+        """
+        Checks if Pacman collides with any pellet in the pellet list.
+
+        :param pelletList: List of pellets in the game.
+        :return: The pellet that was eaten, or None if no collision.
+        """
         for pellet in pelletList:
             if self.collideCheck(pellet):
                 return pellet
-        return None    
-    
-    # def eatPellets(self, pelletList):
-    #     for pellet in pelletList:
-    #         if self.collideCheck(pellet):
-    #             return pellet
-    #     return None    
-    
+        return None
+
     def collide_ghost(self, ghost):
+        """
+        Checks if Pacman collides with a ghost.
+
+        :param ghost: The ghost entity.
+        :return: True if Pacman collides with the ghost, False otherwise.
+        """
         return self.collideCheck(ghost)
-    
+
     def collideCheck(self, other):
+        """
+        Checks collision with another object based on distance.
+
+        :param other: The other entity to check collision with.
+        :return: True if collides, False otherwise.
+        """
         d = self.position - other.position
         dSquared = d.magnitudeSquared()
-        rSquared = (self.collide_radius + other.collide_radius)**2
-        if dSquared <= rSquared:
-            return True
-        return False
-    
-
-    # #TEMP ONLY
-    # def render(self, screen):
-    #     p = self.position.asInt()
-    #     pygame.draw.circle(screen, self.color, p, self.radius)
-    #     print(p)
+        rSquared = (self.collide_radius + other.collide_radius) ** 2
+        return dSquared <= rSquared
